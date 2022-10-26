6f814394f5c934977e188782accff83fbec81b5d
karabaevemir <karabaevemir258@gmail.com>
2022-10-26 14:55:04 +0300

    Commit three

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from aiogram.utils import executor
 from config import bot, dp
 import logging
-from handlers import admin, callback, extra, client
+from handlers import admin, callback, extra, client, notifications
 from database.bot_db import sql_create
-
+import asyncio
 
 async def on_startup(_):
     sql_create()
-
+asyncio.create_task(notifications.scheduler())
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
-
+notifications.register_handlers_notification(dp)
 extra.register_handlers_extra(dp)
 
 
